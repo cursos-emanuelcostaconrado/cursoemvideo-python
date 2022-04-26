7652a91ae95e0005f2d97230f8590f7854b13f45
Emanuel Costa Conrado <dev.conradoemanuelcosta@gmail.com>
2022-04-25 23:15:36 -0300

    Aulas Mundo 01 Concluídas

---
+++ Mundo_01/PythonTeste/aula11.py
@@ -0,0 +1,6 @@
+# print('\033[31m Olá, Mundo!')
+print('\033[1;31;43m Olá, Mundo! \033[m ')
+print('\033[;30;45m Olá, Mundo! \033[m ')
+print('\033[30m Olá, Mundo! \033[m ')
+print('\033[7;30m Olá, Mundo! \033[m ')
+print('\033[37m Olá, Mundo! \033[m ')
